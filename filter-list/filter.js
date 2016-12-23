d96fc65c3fabdc31e155b673c90eaab6793a170e
Leonard Peng <pjw693532857@163.com>
2016-12-23 23:05:07 +0800

    没有匹配项的块的header不显示

--- filter-list/filter.js
+++ filter-list/filter.js
@@ -9,9 +9,20 @@ var filter = function () {
     input.addEventListener('keyup', function (e) {
         var a;
         var filter = e.target.value.toUpperCase();
+        var header = null; // save header li
+        var matched = false; // whether there is a match in one header block
         for (var i = 0; i < li.length; i++) {
             a = li[i].getElementsByTagName('a')[0];
             if (a.className === 'header') {
+                console.log(header, matched);
+                if(header !== null) {
+                    if(!matched) {
+                        // no match in the header block
+                        header.style.display = 'none';
+                    }
+                }
+                header = a;
+                matched = false;
                 continue;
             }
             if (a.innerText.toUpperCase().indexOf(filter) === -1) {
@@ -19,6 +30,7 @@ var filter = function () {
             }
             else {
                 li[i].style.display = 'block';
+                matched = true;
             }
         }
     });
