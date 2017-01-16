f8957f3036baf55c91b8e036a9fddfbb93a7d9fb
Leonard Peng <pjw693532857@163.com>
2017-01-16 18:48:52 +0800

    FIx bug

--- snake/snake.js
+++ snake/snake.js
@@ -2,50 +2,49 @@
  * Created by leonard on 2016/11/7.
  */
 
-let Snake = {}
+let Snake = {};
 
 Snake.game = (function () {
-    let canvas = document.getElementById('canvas')
-    let deadHint = document.getElementById('dead-hint')
-    let score = document.getElementById('score')
-    const ctx = canvas.getContext('2d')
-    const timeInterval = 100
-    const blockSize = 20
-    const width = Math.floor(canvas.offsetWidth / blockSize)
-    const height = Math.floor(canvas.offsetHeight / blockSize)
+    let canvas = document.getElementById('canvas');
+    let deadHint = document.getElementById('dead-hint');
+    let score = document.getElementById('score');
+    const ctx = canvas.getContext('2d');
+    const timeInterval = 100;
+    const blockSize = 20;
+    const width = Math.floor(canvas.offsetWidth / blockSize);
+    const height = Math.floor(canvas.offsetHeight / blockSize);
 
-    let snake
-    let apple
-    let dead = false
+    let snake;
+    let apple;
+    let dead = false;
 
     function init() {
-        snake = Snake.snake(blockSize, height, width)
-        apple = Snake.apple(blockSize, height, width)
-        bindListener()
-        canvas.focus()
-        loop()
+        snake = Snake.snake(blockSize, height, width);
+        apple = Snake.apple(blockSize, height, width);
+        bindListener();
+        canvas.focus();
+        loop();
     }
 
     function loop() {
-        snake.advance()
+        snake.advance();
         if (snake.isDead()) {
-            deadHint.style.visibility = 'visible'
-            dead = true
+            deadHint.style.visibility = 'visible';
+            dead = true;
         } else {
-            let applePosition = apple.getPosition()
+            let applePosition = apple.getPosition();
             let snakeHeadPosition = snake.getHeadPosition()
             if (snakeHeadPosition[0] === applePosition[0] && snakeHeadPosition[1] === applePosition[1]) {
-                console.log('eat an apple')
-                apple.init()
-                snake.grow()
+                apple.init();
+                snake.grow();
                 score.innerText = snake.getScore()
             }
         }
         if (!dead) {
             deadHint.style.visibility = 'hidden'
             ctx.clearRect(0, 0, canvas.offsetWidth, canvas.offsetHeight)
-            apple.draw(ctx)
-            snake.draw(ctx)
+            apple.draw(ctx);
+            snake.draw(ctx);
             setTimeout(loop, timeInterval)
         }
     }
@@ -57,17 +56,21 @@ Snake.game = (function () {
             39: 'right',
             40: 'down',
             32: 'restart'  // space key
-        }
+        };
 
         canvas.addEventListener('keydown', function (event) {
-            let key = event.which
-            let signal = keysToSignal[key]
+            let key = event.which;
+            let signal = keysToSignal[key];
             if (signal === 'restart') {
-                snake.init()
-                apple.init()
+                snake.init();
+                apple.init();
+                deadHint.style.visibility = 'hidden';
+                dead = false;
+                setTimeout(loop, timeInterval);
+                event.preventDefault();
             } else if (signal) {
-                snake.setDirection(signal)
-                event.preventDefault()
+                snake.setDirection(signal);
+                event.preventDefault();
             }
         })
     }
@@ -75,37 +78,38 @@ Snake.game = (function () {
     return {
         init: init
     }
-})()
+})();
 
 Snake.apple = function (blockSize, height, width) {
-    let position
+    let position;
 
-    init()
+    init();
 
     function init() {
         position = generatePosition()
     }
 
     function generatePosition() {
-        let x = Math.floor(Math.random() * width)
-        let y = Math.floor(Math.random() * height)
-        return [x, y]
+        // 存在问题：未判断是否与蛇重叠
+        let x = Math.floor(Math.random() * width);
+        let y = Math.floor(Math.random() * height);
+        return [x, y];
     }
 
     function draw(ctx) {
-        ctx.save()
-        ctx.fillStyle = '#ff2211'
-        ctx.beginPath()
-        let radius = blockSize / 2
-        let x = position[0] * blockSize + radius
-        let y = position[1] * blockSize + radius
-        ctx.arc(x, y, radius, 0, Math.PI * 2, true)
-        ctx.fill()
-        ctx.restore()
+        ctx.save();
+        ctx.fillStyle = '#ff2211';
+        ctx.beginPath();
+        let radius = blockSize / 2;
+        let x = position[0] * blockSize + radius;
+        let y = position[1] * blockSize + radius;
+        ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+        ctx.fill();
+        ctx.restore();
     }
 
     function getPosition() {
-        return position
+        return position;
     }
 
     return {
@@ -113,126 +117,125 @@ Snake.apple = function (blockSize, height, width) {
         draw: draw,
         getPosition: getPosition
     }
-}
+};
 
 Snake.snake = function (blockSize, height, width) {
-    const initLength = 5
-    let posArray = []
-    let direction = 'down'
-    let nextDirection = direction
-    let growSignal = 0
+    const initLength = 5;
+    let posArray = [];
+    let direction = 'down';
+    let nextDirection = direction;
+    let growSignal = 0;
 
-    init()
+    init();
 
     function generatePosition() {
-        let x = Math.floor(Math.random() * width) - initLength
-        let y = Math.floor(Math.random() * height) - initLength
+        let x = Math.floor(Math.random() * width) - initLength;
+        let y = Math.floor(Math.random() * height) - initLength;
         return [x, y]
     }
 
     function init() {
         // reset the params
-        posArray = []
-        direction = 'down'
-        nextDirection = direction
+        posArray = [];
+        direction = 'down';
+        nextDirection = direction;
 
-        let tailPosition
+        let tailPosition;
         do {
-            tailPosition = generatePosition()
+            tailPosition = generatePosition();
         } while (tailPosition[0] < 0 || tailPosition[1] < 0)
 
-        posArray[0] = tailPosition
+        posArray[0] = tailPosition;
         for (let i = 1; i < initLength; i++) {
-            posArray[i] = [posArray[i - 1][0] + 1, posArray[i - 1][1]]
+            posArray[i] = [posArray[i - 1][0] + 1, posArray[i - 1][1]];
         }
     }
 
     function drawSection(ctx, position) {
-        let x = blockSize * position[0]
-        let y = blockSize * position[1]
-        ctx.fillRect(x, y, blockSize, blockSize)
+        let x = blockSize * position[0];
+        let y = blockSize * position[1];
+        ctx.fillRect(x, y, blockSize, blockSize);
     }
 
     function draw(ctx) {
-        ctx.save()
-        ctx.fillStyle = '#112233'
+        ctx.save();
+        ctx.fillStyle = '#112233';
         for (let i = 0; i < posArray.length; i++) {
-            drawSection(ctx, posArray[i])
+            drawSection(ctx, posArray[i]);
         }
-        ctx.restore()
+        ctx.restore();
     }
 
     function setDirection(newDirection) {
-        let allowedDirection
-
+        let allowedDirection;
         // If snake is going up or down, only valid new directions are left or right
         switch (direction) {
             case 'up':
             case 'down':
-                allowedDirection = ['left', 'right']
-                break
+                allowedDirection = ['left', 'right'];
+                break;
             case 'left':
             case 'right':
-                allowedDirection = ['up', 'down']
+                allowedDirection = ['up', 'down'];
         }
         if (allowedDirection.indexOf(newDirection) > -1) {
-            nextDirection = newDirection
+            nextDirection = newDirection;
         }
     }
 
     function advance() {
-        let nextPosition = getHeadPosition()
-        direction = nextDirection
+        let nextPosition = getHeadPosition();
+        direction = nextDirection;
         switch (direction) {
             case 'left':
-                nextPosition[0] -= 1
-                break
+                nextPosition[0] -= 1;
+                break;
             case 'right':
-                nextPosition[0] += 1
-                break
+                nextPosition[0] += 1;
+                break;
             case 'up':
-                nextPosition[1] -= 1
-                break
+                nextPosition[1] -= 1;
+                break;
             case 'down':
-                nextPosition[1] += 1
-                break
+                nextPosition[1] += 1;
+                break;
         }
-        posArray.push(nextPosition)
+        posArray.push(nextPosition);
         if (growSignal > 0) {
-            growSignal -= 1
+            growSignal -= 1;
         } else {
-            posArray.shift()
+            posArray.shift();
         }
     }
 
     function grow() {
-        growSignal += 1
+        growSignal += 1;
     }
 
     function getHeadPosition() {
-        return posArray[posArray.length - 1].slice()
+        return posArray[posArray.length - 1].slice();
     }
 
     function getScore() {
-        return posArray.length - initLength + 1
+        return posArray.length - initLength + 1;
     }
 
     function isDead() {
         // across the wall
-        let headPosition = getHeadPosition()
+        let headPosition = getHeadPosition();
         if (headPosition[0] < 0 || headPosition[1] < 0 ||
             headPosition[0] > width || headPosition[1] > height) {
-            return true
+            return true;
         }
 
         // across snake itself
         for (let i = 0; i < posArray.length - 2; i++) {
             if (headPosition[0] === posArray[i][0] && headPosition[1] == posArray[i][1]) {
-                return true
+                return true;
             }
         }
 
-        return false
+        return false;
     }
 
 
@@ -246,4 +249,4 @@ Snake.snake = function (blockSize, height, width) {
         getHeadPosition: getHeadPosition,
         getScore: getScore
     }
-}
+};
